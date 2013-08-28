4df686a3a0247e58c4182b4d50a0beb1c3e40c5f
Ted-Chang <zgk_1208@yahoo.com.cn>
2013-08-27 22:58:53 -0400

    Fix the bug in update_img.sh

--- update_img.sh
+++ update_img.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+tool_path="tools/make_initrd"
+
 # update the matrix kernel in the floopy image
 #sudo losetup /dev/loop0 ~/vm/matrix/matrix.img
-tools/make_initrd bin/init init bin/crond crond bin/echo echo bin/unit_test unit_test bin/ls ls bin/cat cat bin/clear clear bin/shutdown shutdown bin/mkdir mkdir bin/date date bin/mount mount bin/umount umount bin/crontab crontab bin/initrd
+if [ ! -x "$tool_path"]; then
+    echo "$tool_path not exist or not executable"
+    exit -1
+fi
+
+$tool_path bin/init init bin/crond crond bin/echo echo bin/unit_test unit_test bin/ls ls bin/cat cat bin/clear clear bin/shutdown shutdown bin/mkdir mkdir bin/date date bin/mount mount bin/umount umount bin/crond crond bin/initrd
 sudo mount /dev/loop0 /mnt/matrix
 sudo cp bin/matrix /mnt/matrix
 sudo cp bin/initrd /mnt/matrix
