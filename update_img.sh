50457d32305266d8a638b43aa3745739960c7af5
ted <ted.g.zhang@live.com>
2016-07-31 23:50:41 -0400

    Added grub configuration. Updated shell script.

--- update_img.sh
+++ update_img.sh
@@ -12,14 +12,37 @@ usage="usage: update_img.sh [option]
     -a update both floppy and hard disk"
 
 tool_path="tools/make_initrd"
+image_dir="images"
+mnt_point="images/mnt"
 
-update_prepare()
+# Exit with an error message
+die()
+{
+    echo $@
+    exit -1
+}
+
+check_env()
 {
     # Check whether the tool exists and executable
     if [ ! -x "$tool_path" ]; then
-	echo "$tool_path not exist or not executable"
-	exit -1
+	die "$tool_path not exist or not executable"
     fi
+
+    # Check whether image directory exists
+    if [ ! -d "$image_dir" ]; then
+	die "$image_dir not found"
+    fi
+
+    # Check whether mount point exists
+    if [ ! -d "$mnt_point" ]; then
+	die "$mnt_point not found"
+    fi
+}
+
+update_prepare()
+{
+    echo "Preparing init ramdisk"
 
     $tool_path bin/init init bin/crond crond bin/echo echo bin/unit_test unit_test \
 	bin/ls ls bin/cat cat bin/clear clear bin/shutdown shutdown bin/mkdir mkdir \
@@ -28,66 +51,66 @@ update_prepare()
 
 }
 
+update_files()
+{
+    echo "Updating files"
+
+    sudo rm -f "$mnt_point/matrix"
+    sudo rm -f "$mnt_point/initrd"
+    sudo cp bin/matrix "$mnt_point"
+    sudo cp bin/initrd "$mnt_point"
+
+    sync
+}
+
 # update the matrix kernel in the floopy image
 update_flpy()
 {
-    result=`sudo losetup -a | grep loop0`
-    if [ "$result" == "" ]; then
-	sudo losetup /dev/loop0 images/matrix-flpy.img
+    flpy_img="$image_dir/matrix-flpy.img"
+
+    if [ ! -e "$flpy_img" ]; then
+	die "$flpy_img not found"
     fi
 
     update_prepare
 
-    echo "Copying files to floppy disk"
+    sudo losetup /dev/loop0 "$flpy_img"
 
-    # Mount the root partition onto /mnt
-    sudo mount /dev/loop0 /mnt/matrix/
+    sleep 1s
 
-    sudo rm -f /mnt/matrix/matrix
-    sudo rm -f /mnt/matrix/initrd
-    sudo cp bin/matrix /mnt/matrix/
-    sudo cp bin/initrd /mnt/matrix/
+    # Mount the root partition onto $mnt_point
+    sudo mount /dev/loop0 "$mnt_point"
 
-    sync
+    update_files
 
     # Unmount the root partition
-    sudo umount /mnt/matrix
+    sudo umount "$mnt_point"
 }
 
 # update the matrix kernel in the hard disk image
 update_hd()
 {
-    if [ ! -e "/dev/mapper/hda" ]; then
-        # Loop mount the disk image
-	sudo losetup /dev/loop1 images/matrix-hd.img
-	# Create device mapper node for root device
-	echo '0 101808 linear 7:1 0' | sudo dmsetup create hda
+    hd_img="$image_dir/matrix-hd.img"
+
+    if [ ! -e "$hd_img" ]; then
+	die "$hd_img not found"
     fi
 
     update_prepare
 
-    echo "Mounting hard disk image file"
-    # Mount the root partition onto /mnt
-    sudo mount /dev/mapper/hda /mnt/matrix
-    
-    echo "Copying files to hard disk"
-    sudo rm -f /mnt/matrix/matrix
-    sudo rm -f /mnt/matrix/initrd
-    # Copy the files to the disk image
-    sudo cp bin/matrix /mnt/matrix/
-    sudo cp bin/initrd /mnt/matrix/
+    sudo losetup /dev/loop1 "$hd_img"
 
-    # Sleep for 1 seconds as device mapper will be busy if we unmount it now
     sleep 1s
 
-    # Sync data to the image file
-    sync
+    echo "Mounting hard disk image file"
+
+    # Mount the root partition onto /mnt
+    sudo mount /dev/loop1 "$mnt_point"
+    
+    update_files
 
     # Umount the root partition
-    sudo umount /mnt/matrix
-
-    # Remove the /dev/mapper/hda device
-    sudo dmsetup remove /dev/mapper/hda
+    sudo umount "$mnt_point"
 
     # Detach loopback device
     sudo losetup -d /dev/loop1
@@ -98,12 +121,15 @@ case "$1" in
 	echo "$usage"
 	;;
     "-f")
+	check_env
 	update_flpy
 	;;
     "-d")
+	check_env
 	update_hd
 	;;
     "-a")
+	check_env
 	update_flpy
 	update_hd
 	;;
